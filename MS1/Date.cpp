67a28b5435e14a0104fa577e6cb1c61870b32063
Baihua Chen <bchen102@myseneca.ca>
2024-06-26 07:53:08 -0400

    2024-06-26 OOP MS1 processing windows

--- MS1/Date.cpp
+++ MS1/Date.cpp
@@ -23,6 +23,7 @@
 using namespace std;
 #include "Date.h"
 namespace seneca {
+
    bool Date::validate() {
       errCode(NO_ERROR);
       if (m_year < MIN_YEAR || m_year > m_CUR_YEAR + 1) {
@@ -36,17 +37,20 @@ namespace seneca {
       }
       return !bad();
    }
-   int Date::mdays()const {
+
+   int Date::mdays() const {
       int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
       int mon = m_mon >= 1 && m_mon <= 12 ? m_mon : 13;
       mon--;
       return days[mon] + int((mon == 1) * ((m_year % 4 == 0) && (m_year % 100 != 0)) || (m_year % 400 == 0));
    }
-   int Date::systemYear()const {
+
+   int Date::systemYear() const {
       time_t t = time(NULL);
       tm lt = *localtime(&t);
       return lt.tm_year + 1900;
    }
+
    void Date::setToToday() {
       time_t t = time(NULL);
       tm lt = *localtime(&t);
@@ -55,6 +59,7 @@ namespace seneca {
       m_year = lt.tm_year + 1900;
       errCode(NO_ERROR);
    }
+
    int Date::daysSince0001_1_1()const { // Rata Die day since 0001/01/01 
       int ty = m_year;
       int tm = m_mon;
@@ -64,28 +69,33 @@ namespace seneca {
       }
       return 365 * ty + ty / 4 - ty / 100 + ty / 400 + (153 * tm - 457) / 5 + m_day - 306;
    }
-   Date::Date() :m_CUR_YEAR(systemYear()) {
+
+   Date::Date() : m_CUR_YEAR(systemYear()) {
       setToToday();
    }
+
    Date::Date(int year, int mon, int day) : m_CUR_YEAR(systemYear()) {
       m_year = year;
       m_mon = mon;
       m_day = day;
       validate();
    }
-   const char* Date::dateStatus()const {
+
+   const char* Date::dateStatus() const {
       return DATE_ERROR[errCode()];
    }
-   int Date::currentYear()const {
+
+   int Date::currentYear() const {
       return m_CUR_YEAR;
    }
+
    void Date::errCode(int readErrorCode) {
       m_ErrorCode = readErrorCode;
    }
-   int Date::errCode()const {
+   int Date::errCode() const {
       return m_ErrorCode;
    }
-   bool Date::bad()const {
+   bool Date::bad() const {
       return m_ErrorCode != 0;
    }
   
